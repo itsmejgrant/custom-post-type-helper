d7bcd32ca68d70d3d37015dc578953ce93a47eec
Joe Grant <jmgrant1995@gmail.com>
2022-08-16 10:31:31 +0800

    Update CustomPostType.php

--- src/CustomPostType.php
+++ src/CustomPostType.php
@@ -138,7 +138,7 @@ class CustomPostType
             "show_in_admin_bar" => true,
             "menu_position" => 5,
             "can_export" => true,
-            "has_archive" => true,
+            "has_archive" => false,
             "exclude_from_search" => false,
             "publicly_queryable" => true,
             "capability_type" => "page",
